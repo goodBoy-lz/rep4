bb16afd91f3837d5666a0052ac6a1f2f14dbf496
lizheng <1364094745@qq.com>
2020-05-05 22:10:47 +0800

    第一次提交

--- ceshi/git/src/com/itheima/demo/Person.java
+++ ceshi/git/src/com/itheima/demo/Person.java
@@ -1,7 +1,8 @@
 package com.itheima.demo;
-
+//我是一个老屌丝
 public class Person {
     public static void main(String[] args) {
         System.out.println("夯哥最后一天！");
+        System.out.println("我下班了，提交数据");
     }
 }
